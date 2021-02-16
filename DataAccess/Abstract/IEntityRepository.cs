79f1a5599fe866699cd155e641675e29e032e5ca
mumenrider27 <afrke92505@gmail.com>
2021-02-16 22:54:06 +0300

    IEntityRepository constraints added

--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRepository<T>
+    // generic constraint
+    // class: reference type
+    // IEntity: IEntity or anything that implements IEntity
+    // new: Can't be an interface | has to be a non-abstract type
+    public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
