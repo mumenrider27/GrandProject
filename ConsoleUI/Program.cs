1141e52f69c40b22178d7ae0ef00c36f5cb09be7
mumenrider27 <afrke92505@gmail.com>
2021-02-18 11:44:23 +0300

    Added DTO / DTO implementation

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,8 +11,9 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            // ProductTest();
-            CategoryTest();
+            ProductTest();
+            // IoC
+            // CategoryTest();
         }
 
         private static void CategoryTest()
@@ -28,9 +29,9 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40, 100))
+            foreach (var product in productManager.GetProductDetails())
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(product.ProductName + "/" + product.CategoryName);
             }
         }
     }
