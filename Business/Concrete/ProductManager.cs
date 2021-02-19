400f992091b43f2ead5a6b10e14b730cd55a2568
mumenrider27 <afrke92505@gmail.com>
2021-02-19 11:05:33 +0300

    WebAPI-AddSingleton

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -34,10 +34,10 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour == 22)
-            {
-                return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
-            }
+            //if (DateTime.Now.Hour == 22)
+            //{
+            //    return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
+            //}
 
             // Business code // A business class does not instantiate (new) other classes.
             return new SuccessDataResult<List<Product>> (_productDal.GetAll(),Messages.ProductsListed);
--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
-﻿using Entities.Concrete;
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,14 +15,24 @@ namespace WebAPI.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
+        // Loosely coupled
+        // naming convention
+        // IoC Container -- Inversion of Control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+
         [HttpGet]
         public List<Product> Get()
         {
-            return new List<Product>
-            {
-                new Product{ProductId=1,ProductName="Product1"},
-                new Product{ProductId=2,ProductName="Product2"}
-            };
+            // Dependency chain --
+            
+            var result = _productService.GetAll();
+            return result.Data;
         }
     }
 }
